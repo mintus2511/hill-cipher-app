2b073dc46e604ac63bfb2ffa2429fef5377de287
mintus2511 <sallyminhtu251505@gmail.com>
2025-05-04 10:55:58 +0700

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -120,7 +120,7 @@ if st.button("🔁 Run Cipher") and key_matrix is not None:
 # --- Hill++ Cipher ---
 st.markdown("---")
 st.subheader("🧬 Hill++ Encryption & Decryption (Side-by-Side)")
-left_col, right_col = st.columns([1, 0.15, 1])
+left_col, spacer, right_col = st.columns([1, 0.15, 1])
 
 with left_col:
     st.markdown("### 🔐 Encrypt with Hill++")
